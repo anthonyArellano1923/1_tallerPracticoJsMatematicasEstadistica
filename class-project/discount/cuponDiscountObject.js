3a0378797b98dbde8b35d0f1c93a3c9a5699d8d9
anthonyArellano1923 <163557083+anthonyArellano1923@users.noreply.github.com>
2025-01-09 19:59:01 -0300

    Class 5 challenge, cupons as objects.

---
+++ class-project/discount/cuponDiscountObject.js
@@ -0,0 +1,63 @@
+const inputPrice = document.querySelector('#price')
+const inputCupon = document.querySelector('#cupon')
+const applyButton = document.querySelector('#applyDiscount')
+const discountedPrice = document.querySelector('#priceDiscounted')
+const cuponsDB = {
+  'winter-discount' : 10,
+  'god-discount' : 15,
+  'tarnished-discount' : 20,
+  'ashen-one-discount' : 25,
+}
+
+function applyDiscount(cuponDiscount) {
+  const originalPrice = inputPrice.value
+  if (originalPrice > 0 ) {
+    const priceDiscounted = (originalPrice * (100 - cuponDiscount)) / 100
+    discountedPrice.innerText = 'Your price got a ' + cuponDiscount + '% discount, now you pay ' + priceDiscounted + '$'
+  } else {
+    discountedPrice.innerText = 'You must digit a price that must be higher than 0.'
+  }
+}
+
+function getCupon() {
+  const userCupon = inputCupon.value
+  const originalPrice = inputPrice.value
+  let found = false
+
+  if (originalPrice == '') {
+    discountedPrice.innerText = 'You must digit a price.'
+    return
+  }
+  for(let cupon in cuponsDB) {
+    if (userCupon == cupon) {
+      applyDiscount(cuponsDB[cupon])
+      found = true
+    }
+    
+    
+  }
+  if(!found) {
+    discountedPrice.innerText = 'Non-valid cupon, input a valid one to get the discount'
+ 
+  }
+
+  // if (userCupon == 'winter-discount') {
+  //   applyDiscount(10)
+  // } else if (userCupon == 'god-discount') {
+  //   applyDiscount(15)
+  // } else if (userCupon == 'tarnished-discount') {
+  //   applyDiscount(20)
+  // } else if (userCupon == 'ashen-one-discount') {
+  //   applyDiscount(25)
+  // } else {
+  //   applyDiscount(15)
+  //   discountedPrice.innerText = 'Non-valid cupon, input a valid one to get the discount'
+  // }
+}
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  applyButton.addEventListener('click', () => getCupon())
+});
+
+
