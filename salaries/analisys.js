45a1826e831d0b2b2dd71a622f5c0db694906b99
anthonyArellano1923 <163557083+anthonyArellano1923@users.noreply.github.com>
2025-01-21 16:11:05 -0300

    Class 12, challenges of reorganizing workers info and geting yearly median of a company.

--- salaries/analisys.js
+++ salaries/analisys.js
@@ -1,4 +1,5 @@
 const workers = salarios
+const companies = getCompaniesInfo(workers)
 
 function findPerson(personToFind) { //Requieres person 'name'
   return workers.find((worker) => worker.name == personToFind)
@@ -12,9 +13,95 @@ function calculateWorkerMedian(person) { //Requieres person 'name'
 }
 
 function projectingSalaries(person) {
-  const worker = findPerson(person) //Finds the subject
-  const workerSalaries = worker.trabajos.map((job) => job.salario)//Creates a list of subjects salaries
-  const salariesDeltas = workerSalaries.map((salary, index) => workerSalaries[index + 1] - salary).filter(delta => delta !== null && !Number.isNaN(delta)) // Create a list of the deltas between salaries, excluding null or NaN values.
-  const linearProjection = YnohtsMath.average(salariesDeltas) * 100 / workerSalaries[workerSalaries.length - 1] //“Calculate the average of the deltas and the percentage of salary variation based on the average.”
-  return linearProjection.toFixed(2) + '%'
+  const worker = findPerson(person)
+  const workerSalaries = worker.trabajos.map((job) => job.salario)
+  const salariesDeltas = workerSalaries.map((salary, index) => workerSalaries[index + 1] - salary).filter(delta => delta !== null && !Number.isNaN(delta)) 
+  const linearProjection = YnohtsMath.calculateMedian(salariesDeltas) / workerSalaries[workerSalaries.length - 1]
+  const newSalary = workerSalaries[workerSalaries.length - 1] + workerSalaries[workerSalaries.length - 1] * linearProjection
+  console.log('Actual salary: ', workerSalaries[workerSalaries.length - 1])
+  return newSalary
 }
+
+//Análisis empresarial
+
+function selfCompaniesAnalysis(workers){
+/*{
+-Primero, debo crear una lista de empresas. Para ello debo acceder a cada persona y ver en qué empresa ha trabajado
+}*/
+  function getCompanies(workers){ 
+    listOfCompanies = []
+    workers.forEach(worker => {
+      worker.trabajos.forEach(job => {
+          if (!listOfCompanies.includes(job.empresa)){
+            listOfCompanies.push(job.empresa)
+          }
+      })
+    })
+    return listOfCompanies
+  }
+
+/*{
+-Segundo, por cada empresa, verificar si el trabajador estuvo en la empresa en la que estamos iterando. Si es así, obtener los salarios del año correspondiente.
+}*/
+
+  function getWorkersSalaries(workers, companyName, yearToReview) {
+    let workersSalaries = []
+    workers.forEach(worker => {
+      worker.trabajos.forEach(job => {
+        if (job.year == yearToReview && job.empresa == companyName){
+          workersSalaries.push(job.salario)
+        }
+      })
+    })
+    return workersSalaries
+  }
+  
+  const startingYear = 2018
+  let companiesToAnalyze = getCompanies(workers)
+  let companiesAnalized = {}
+
+  for (let i = 0; i < companiesToAnalyze.length; i++) {
+    let companyName = companiesToAnalyze[i]
+    if (!companiesToAnalyze[companyName]){
+      companiesAnalized[companyName] = {}
+    }
+    for (let j = startingYear; j < 2024; j++){
+      let yearToReview = j
+      let salaries = getWorkersSalaries(workers, companyName, yearToReview)
+      companiesAnalized[companyName][yearToReview] = salaries
+    }
+  }
+  
+  return companiesAnalized
+}
+
+function getCompaniesInfo(workers) {
+  let companies = {}
+  workers.forEach(person => {
+    person.trabajos.forEach(job => {
+      if (!companies[job.empresa]) {
+        companies[job.empresa] = {}
+      }
+      if(!companies[job.empresa][job.year]){
+        companies[job.empresa][job.year] = []
+      }
+      companies[job.empresa][job.year].push(job.salario)
+    })
+  })
+  return companies
+}
+
+function yearlyCompanySalaryMedian(company, year) {
+  if(companies[company]){
+    if(companies[company][year]){
+      const yearlyMedian = YnohtsMath.calculateMedian(companies[company][year])
+      return yearlyMedian
+    }
+    else {
+      return `En el año ${year} la empresa ${company} no registra salarios pagados.`
+    }
+  } else {
+    return `La empresa ${company} no se encuentra en la base de datos`
+  }
+}
+
